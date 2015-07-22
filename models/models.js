1ce5314f678553349ce631714dacdbc152800b00
joaquin <jgdaimiel@users.noreply.github.com>
2015-07-22 19:54:23 +0200

    creada la base de datos usando SQLite y modificado el controlador para usarla

---
+++ models/models.js
@@ -0,0 +1,23 @@
+var path = require('path');
+
+//Cargar modelo ORM
+var Sequelize = require('sequelize');
+
+//usar BBDD SQLite
+var sequelize = new Sequelize(null, null, null, {dialect: "sqlite", storage: "quiz.sqlite"});
+
+//importar la definición de la tabla Quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+
+exports.Quiz = Quiz; //exportar definición de tabla Quiz
+
+//sequelize.sync() crea e inicializa la tabla de preguntas en DB
+sequelize.sync().then(function() {
+  //then(..) ejecuta el manejador una vez creada la tabla
+  Quiz.count().then(function(count) {
+    if(count === 0) { //la tabla se inicializa solo si está vacía
+      Quiz.create({ pregunta: 'Capital de Italia', respuesta: 'Roma'}).then(function() {
+        console.log('Base de datos inicializada')});
+    }
+  });
+});
