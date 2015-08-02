63fdc4046516242c50567d03cfad91c4a6caa46a
joaquin <jgdaimiel@users.noreply.github.com>
2015-08-02 19:31:45 +0200

    validación en crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -17,13 +17,13 @@ exports.load = function(req, res, next, quizId) {
 //GET /quizes
 exports.index = function(req, res){
 	models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/index', {quizes: quizes});
+		res.render('quizes/index', {quizes: quizes, errors: []});
 	});
 };
 
 //GET /quizes/:quizId
 exports.show = function(req, res){
-		res.render('quizes/show', { quiz: req.quiz });
+		res.render('quizes/show', { quiz: req.quiz, errors: [] });
 };
 
 //GET quizes/:id/answer
@@ -32,24 +32,32 @@ exports.answer = function(req, res){
 		if(req.query.respuesta === req.quiz.respuesta) {
 			resultado = 'Correcto';
 		}
-		res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+		res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: []});
 };
 
 //GET /quizes/new
 exports.new = function(req, res){
 	var quiz = models.Quiz.build({pregunta: "Pregunta", respuesta: "Respuesta"}); //crea objeto quiz
-	res.render('quizes/new', {quiz: quiz});
+	res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 //POST /quizes/create
 exports.create = function(req, res){
 	var quiz = models.Quiz.build(req.body.quiz);
-	//guarda en BD los campos pregunta y respuesta de quiz
-	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-		res.redirect('/quizes');
-	});	 //Redirección HTTP (URL relativo) lista de preguntas
-}
+
+	quiz.validate().then(function(err){
+		if(err) {
+			res.render('quizes/new', {quiz: quiz, errors: err.errors});
+		}
+		else {
+			//guarda en BD los campos pregunta y respuesta de quiz
+			quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+				res.redirect('/quizes');
+			});	 //Redirección HTTP (URL relativo) lista de preguntas
+		}
+	});
+};
 
 exports.author = function(req, res){
-	res.render('quizes/author', {autor: 'Joaquin'});
+	res.render('quizes/author', {autor: 'Joaquin', errors: []});
 };
